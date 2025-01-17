049559c23bcd3bbfd505d7b3d8e12b2afd8d9e1b
someoneReallyDumb <poleshhhuknatalija@gmail.com>
2025-01-18 00:58:28 +0700

    create player

---
+++ monogame/Classes/Player.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace monogame.Classes
+{
+    internal class Player
+    {
+    }
+}
